3aa689dae8b9378846742e998f8d25eab69f09e5
Daniel Gehn <inad.gamer@gmail.com>
2016-01-04 11:28:58 +0100

    Added sample Testeditor Project for Appium

--- appium/src/main/java/org/testeditor/fixture/appium/AbstractAppiumFixture.java
+++ appium/src/main/java/org/testeditor/fixture/appium/AbstractAppiumFixture.java
@@ -5,7 +5,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
